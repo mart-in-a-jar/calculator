4f3b5f3657c214b2540c8f771cf58814dfead97e
Martin <Martin@Jarneid.no>
2022-03-15 15:50:43 +0100

    Add basic functions and layout

---
+++ script.js
@@ -0,0 +1,28 @@
+function add(x, y) {
+    return x + y;
+}
+
+function subtract(x, y) {
+    return x - y;
+}
+
+function multiply(x, y) {
+    return x * y;
+}
+
+function divide(x, y) {
+    return x / y;
+}
+
+function operate(operator, num1, num2) {
+    switch (operator) {
+        case "+":
+            return add(num1, num2);
+        case "-":
+            return subtract(num1, num2);
+        case "*":
+            return multiply(num1, num2);
+        case "/":
+            return divide(num1, num2);
+    }
+}
